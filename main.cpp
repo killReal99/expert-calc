81faa41ef251f09cd3d063c96be26610c4808803
Kirill <kirilllatyshov99@gmail.com>
2024-01-10 09:44:44 -0500

    First commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Expert.h"
+
+using namespace std;
+
+int main()
+{
+    float beta = 0.5;
+    float lambda = 0.5;
+
+    
+
+    cout<<"beta is " << beta << endl;
+    cout<<"lambda is " << lambda << endl;
+
+    return 0;
+}
